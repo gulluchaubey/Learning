d4304d951eea7b3d83380c21ee8681d3f7334b39
divya <gulluchaubey1096@gmail.com>
2022-07-22 18:23:43 +0530

    navigation home minor changes

--- app/src/main/java/com/example/learning/navigationTablayoutDemo/HomeActivity.kt
+++ app/src/main/java/com/example/learning/navigationTablayoutDemo/HomeActivity.kt
@@ -1,6 +1,9 @@
 package com.example.learning.navigationTablayoutDemo
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
@@ -12,7 +15,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding : ActivityHomeBinding
     private lateinit var appBarConfiguration :AppBarConfiguration
@@ -40,10 +43,39 @@ class HomeActivity : AppCompatActivity() {
                 //Display up icon when not in top-level destination
                 //Update title text based on the destination’s label
         setupActionBarWithNavController(navController,appBarConfiguration)
+        binding.navView.setNavigationItemSelectedListener(this)
+
+        navController.addOnDestinationChangedListener {_,destination,_ ->
+            if(destination.id==R.id.aboutFragment || destination.id== R.id.settingFragment)
+                binding.bottomNav.visibility=View.GONE
+            else
+                binding.bottomNav.visibility=View.VISIBLE
+        }
     }
 
     //to handle hamburger click
     override fun onSupportNavigateUp(): Boolean {
         return findNavController(R.id.container_fragment).navigateUp(appBarConfiguration)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share->{
+                var share = Intent(Intent.ACTION_SEND)
+                share.type="text/plain"
+                share.putExtra(Intent.EXTRA_TEXT,"testing sharing")
+                startActivity(Intent.createChooser(share,"heading"))
+                binding.drawerLayout.close()
+            }
+            R.id.aboutFragment->{
+                findNavController(R.id.container_fragment).navigate(R.id.aboutFragment)
+                binding.drawerLayout.close()
+            }
+            R.id.settingFragment->{
+                findNavController(R.id.container_fragment).navigate(R.id.settingFragment)
+                binding.drawerLayout.close()
+            }
+        }
+        return true
     }
 }
